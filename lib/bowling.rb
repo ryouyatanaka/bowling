bc237e85e1bd46a64372c51759234d00641bae20
ryouya tanaka <ryouramurakiri@gmail.com>
2017-01-18 06:54:43 +0000

    ストライクの処理

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -14,17 +14,33 @@ class Bowling
     
     def calc_score
         @index = 0
-        while @score.size > @index do
+        @frame = 1
+        while @frame <= 10  do
             pin1 = @score[@index]
-            pin2 = @score[@index+1]
-            pin3 = @score[@index+2] if @index <  @score.size
-            @total += pin1
-            @total += pin2
-           # p @pin1 + @pin2
-            @total += pin3 if pin1 + pin2 == 10 && pin3 != nil
-    
-            @index += 2
-            #p "total: #{@total}"
+            pin2 = @score[@index+1] if @index+1 <= @score.size
+            pin3 = @score[@index+2] if @index+2 <= @score.size 
+            #ストライクの処理
+            if pin1 == 10 && pin2!= nil && pin3 != nil  then
+                @total += pin1 + pin2 + pin3
+                @frame += 1
+                @index += 1
+            elsif  pin1 == 10 && pin2 != nil then
+                @total += pin1 + pin2 
+                @frame += 1
+                @index += 1 
+            elsif pin1 == 10 then
+                @total += pin1
+                @frame += 1
+                @index += 1      
+            #ここまで    
+            else
+                @total += pin1
+                @total += pin2
+           
+                @total += pin3 if pin1 + pin2 == 10 && pin3 != nil
+                @frame += 1
+                @index += 2
+            end
         end
     end
 end
