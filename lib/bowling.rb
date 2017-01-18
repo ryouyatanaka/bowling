2067535bdba28879f3b8a292b1097d6ed25393ce
ryouya tanaka <ryouramurakiri@gmail.com>
2017-01-18 05:58:07 +0000

    @frameを@indexに修正

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -13,20 +13,18 @@ class Bowling
     end
     
     def calc_score
-        @frame = 0
-        while @score.size > @frame do
-            pin1 = @score[@frame]
-            pin2 = @score[@frame+1]
-            pin3 = @score[@frame+2] if @frame <  @score.size
+        @index = 0
+        while @score.size > @index do
+            pin1 = @score[@index]
+            pin2 = @score[@index+1]
+            pin3 = @score[@index+2] if @index <  @score.size
             @total += pin1
             @total += pin2
            # p @pin1 + @pin2
             @total += pin3 if pin1 + pin2 == 10 && pin3 != nil
     
-            @frame += 2
+            @index += 2
             #p "total: #{@total}"
         end
-        
-        @total
     end
 end
