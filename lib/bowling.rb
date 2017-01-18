0c8f74de8075ecf9577b9f14ffcbb2f8582d3826
ryouya tanaka <ryouramurakiri@gmail.com>
2017-01-18 05:32:05 +0000

    spare_system

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,13 +1,32 @@
 class Bowling
     def initialize 
-        @score = 0
+        @score = []
     end
     
     def add_score(pins)
-        @score += pins
+        @score << pins
     end
     
     def total_score
-        @score
+        @total
+    end
+    
+    def calc_score
+        @total = 0
+        @frame = 0
+        while @score.size > @frame do
+            pin1 = @score[@frame]
+            pin2 = @score[@frame+1]
+            pin3 = @score[@frame+2] if @frame <  @score.size
+            @total += pin1
+            @total += pin2
+           # p @pin1 + @pin2
+            @total += pin3 if pin1 + pin2 == 10 && pin3 != nil
+    
+            @frame += 2
+            #p "total: #{@total}"
+        end
+        
+        @total
     end
 end
